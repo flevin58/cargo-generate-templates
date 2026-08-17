964ca4ed21fd6791ea6fd11203864ed865437dbe
Fernando Levin <flevin58@gmail.com>
2026-08-17 11:44:32 +0200

    Fixes

--- egui-application/src/main.rs
+++ egui-application/src/main.rs
@@ -5,7 +5,8 @@ use app::MyApp;
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default()
+        .with_inner_size([320.0, 240.0]),
         ..Default::default()
     };
     eframe::run_native(
